c3bbadfca90ab32d0f3b9f39faea7619d1f6d4b3
Hooman Tirasfar <hooman@Hoomans-MacBook-Pro.local>
2017-02-06 21:58:25 +0330

    syles index page

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,7 @@ class Location < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
 
-	  has_attached_file :image, styles: { medium: "460x225>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	  has_attached_file :image, styles: { medium: "457x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   		validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
