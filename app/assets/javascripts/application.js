91817a5d9ee44f3db3b8fb4340b40a045aab15ce
Hooman Tirasfar <hooman@Hoomans-MacBook-Pro.local>
2017-02-06 08:02:58 +0330

    syles show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
+}, 4000);
